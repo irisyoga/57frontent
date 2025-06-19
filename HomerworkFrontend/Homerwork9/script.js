aab300002146c16eb2cfd9261b59aecad589ca76
Iryna_Haber <gaber.ira@gmail.com>
2025-06-19 22:36:06 +0200

    Hm9: JS Задачи - готово

---
+++ HomerworkFrontend/Homerwork9/script.js
@@ -0,0 +1,34 @@
+
+let myNumber = 100 / (25 % 3);
+console.log(myNumber);
+
+let myString =  "Hello, " + "world" + "!" + "2024.";
+console.log(myString);
+
+let isHuman = 42 == "42";
+console.log(isHuman);
+
+let myNumber1 = parseInt("123smth");
+console.log(myNumber1);
+
+let age = 18;
+let hasPassport = true;
+console.log(hasPassport);
+
+let canTravel = (age >= 18 &&  hasPassport) ? "Вы можете путешествовать ✈️"
+  : "Путешествие невозможно ❌";
+  console.log(canTravel);
+
+let inputString = "500€";
+let price = parseInt("500€");
+console.log(price);
+
+let randomNumber = Math.floor(Math.random() * 50) + 1;
+console.log(randomNumber);
+
+let isEven = randomNumber % 2 === 0;
+console.log(isEven);
+
+
+
+ 
